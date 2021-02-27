418809764a887318866c07372547bdca5afdae2b
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-02-27 13:01:18 -0300

    arruma variavel da diagonal superior direita e realiza movimento diagonal superior esquerda

--- _bloco4/chess-move/queen-attack.js
+++ _bloco4/chess-move/queen-attack.js
@@ -1,8 +1,8 @@
-let queenPositionLine = 7;
-let queenPositionColumn = 3;
+let queenPositionLine = 4;
+let queenPositionColumn = 4;
 
-let targetPositionLine = 8;
-let targetPositionColumn = 4;
+let targetPositionLine = 5;
+let targetPositionColumn = 3;
 
 let executeAttack = false;
 
@@ -11,14 +11,28 @@ if ((queenPositionLine === targetPositionLine) || (queenPositionColumn === targe
   executeAttack = true;
 };
 
-// Superior Direito
+// Diagonal Superior Direita
 for (let supDir = 1; supDir < 8; supDir += 1) {
-  queenPositionLine = queenPositionLine + supDir;
-  queenPositionColumn = queenPositionColumn + supDir;
-  if (queenPositionLine > 8 || queenPositionColumn > 8) {
+  let lineQueen = queenPositionLine + supDir;
+  let columnQueen = queenPositionColumn + supDir;
+
+  if (lineQueen > 8 || columnQueen > 8) {
     break;
-  } else if ((queenPositionLine == targetPositionLine) && (queenPositionColumn == targetPositionColumn)) {
+  } else if ((lineQueen === targetPositionLine) && (columnQueen === targetPositionColumn)) {
     executeAttack = true;
   }
 };
+
+// Diagonal Superior Esquerda
+for (let supEsq = 1; supEsq < 8; supEsq += 1) {
+  let lineQueen = queenPositionLine + supEsq;
+  let columnQueen = queenPositionColumn - supEsq;
+
+  if (lineQueen > 8 || columnQueen < 1) {
+    break;
+  } else if ((lineQueen == targetPositionLine) && (columnQueen == targetPositionColumn)) {
+    executeAttack = true;
+  }
+};
+
 console.log(executeAttack);
