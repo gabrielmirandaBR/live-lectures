eb147fe83ee4e28fbf455a72755c0a4caf6318ca
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-02-27 13:07:27 -0300

    realiza movimento diagonal inferior direita

--- _bloco4/chess-move/queen-attack.js
+++ _bloco4/chess-move/queen-attack.js
@@ -1,8 +1,8 @@
 let queenPositionLine = 4;
 let queenPositionColumn = 4;
 
-let targetPositionLine = 5;
-let targetPositionColumn = 3;
+let targetPositionLine = 3;
+let targetPositionColumn = 5;
 
 let executeAttack = false;
 
@@ -31,6 +31,18 @@ for (let supEsq = 1; supEsq < 8; supEsq += 1) {
   if (lineQueen > 8 || columnQueen < 1) {
     break;
   } else if ((lineQueen == targetPositionLine) && (columnQueen == targetPositionColumn)) {
+    executeAttack = true;
+  }
+};
+
+// Diagonal Inferior Direita
+for (infDir = 1; infDir < 8; infDir += 1) {
+  let lineQueen = queenPositionLine - infDir;
+  let columnQueen = queenPositionColumn + infDir;
+
+  if (lineQueen < 1 || columnQueen > 8) {
+    break;
+  } else if ((lineQueen === targetPositionLine) && (columnQueen === targetPositionColumn)) {
     executeAttack = true;
   }
 };
