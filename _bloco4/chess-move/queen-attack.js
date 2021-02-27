8fc696cdb7d8310d166e3836a67749bb2a667a6c
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-02-27 11:48:57 -0300

    ataque horizontal, vertical e superior direito

--- _bloco4/chess-move/queen-attack.js
+++ _bloco4/chess-move/queen-attack.js
@@ -1,12 +1,24 @@
 let queenPositionLine = 7;
 let queenPositionColumn = 3;
 
-let targetPositionLine = 7;
+let targetPositionLine = 8;
 let targetPositionColumn = 4;
 
 let executeAttack = false;
 
-if(queenPositionLine === targetPositionLine || queenPositionColumn === targetPositionColumn) {
+// Horizontal ou Vertical
+if ((queenPositionLine === targetPositionLine) || (queenPositionColumn === targetPositionColumn)) {
   executeAttack = true;
+};
+
+// Superior Direito
+for (let supDir = 1; supDir < 8; supDir += 1) {
+  queenPositionLine = queenPositionLine + supDir;
+  queenPositionColumn = queenPositionColumn + supDir;
+  if (queenPositionLine > 8 || queenPositionColumn > 8) {
+    break;
+  } else if ((queenPositionLine == targetPositionLine) && (queenPositionColumn == targetPositionColumn)) {
+    executeAttack = true;
+  }
 };
 console.log(executeAttack);
