0b579f9bdd66b820744532127ea9c47d89dce8b2
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-02-27 14:45:14 -0300

    movimento bispo diagonal superior esquerda

--- _bloco4/chess-move/bishop-attack.js
+++ _bloco4/chess-move/bishop-attack.js
@@ -1,8 +1,8 @@
 let bishopPositionLine = 4;
 let bishopPositionColumn = 4;
 
-let targetPositionLine = 8;
-let targetPositionColumn = 8;
+let targetPositionLine = 6;
+let targetPositionColumn = 2;
 
 let executeAttack = false;
 
@@ -17,4 +17,17 @@ for (let supDir = 1; supDir < 8; supDir += 1) {
     executeAttack = true;
   }
 };
+
+// Diagonal Superior Esquerda
+for (let supEsq = 1; supEsq < 8; supEsq += 1) {
+  let lineBishop = bishopPositionLine + supEsq;
+  let columnBishop = bishopPositionColumn - supEsq;
+
+  if(lineBishop > 8 || columnBishop < 1) {
+    break;
+  } else if ((lineBishop === targetPositionLine) && (columnBishop === targetPositionColumn)) {
+    executeAttack = true;
+  }
+};
+
 console.log(executeAttack);
