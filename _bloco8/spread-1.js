398424ebaf9ca1b67c70f4fd34425d14e66cfae7
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-04-08 09:04:02 -0300

    add exercicios spread

---
+++ _bloco8/spread-1.js
@@ -0,0 +1,8 @@
+const names = ['Icaro', 'Thiago', 'Fabio', 'Malbathan', 'Duda', 'Aline'];
+
+// console.log(names[0], names[1], names[2], names[3], names[4], names[5]);
+console.log(names);
+
+// espalhar
+
+console.log([...names]);
